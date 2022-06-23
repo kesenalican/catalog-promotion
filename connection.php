1b372f29bbf4002ce14c15b901b45f73fde900b9
kesenalican <46851204+kesenalican@users.noreply.github.com>
2022-06-23 12:26:27 +0300

    some changes

--- connection.php
+++ connection.php
@@ -1,20 +1,9 @@
 <?php
 
-// $db_host='localhost';
-// $db_user='root';
-// $db_pass='';
-// $db_name='palucico_banyo';
-
 $db_host='localhost';
-$db_user='palucico_banyo';
-$db_pass='VIr!S$B^?ZO-';
+$db_user='root';
+$db_pass='';
 $db_name='palucico_banyo';
-
-
-// define("DB_HOST", 'localhost');
-// define("DB_USER", 'palucico_banyo');
-// define("DB_PASS", 'VIr!S$B^?ZO-');
-// define("DB_NAME", 'palucico_banyo');
 
 
 
